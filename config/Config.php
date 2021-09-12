7d42ccff16a8dc0ec8aba40ce7436cabad89466b
Carlos-Capriel <87543567+Carlos-Capriel@users.noreply.github.com>
2021-09-11 20:32:45 -0500

    arreglando posible error de controlador no encontrado

--- config/Config.php
+++ config/Config.php
@@ -1,5 +1,5 @@
 <?php
-    const BASE_URL = "https://consultoriosmedicos.herokuapp.com"; //direccion local
+    const BASE_URL = "https://consultoriosmedicos.herokuapp.com?url=home"; //direccion local
     const LIBS = "Libraries/";
     const VIEWS = "Views/";
 ?>
