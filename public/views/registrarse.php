5e445ba6a6c1c98d6aeba17fd4c107d3ed43a92d
Reyna <al060872@uacam.mx>
2021-09-15 00:23:44 -0500

    Cree una sub carpeta de la carpeta public, y agregue los archivos login y registrase con extension de php

---
+++ public/views/registrarse.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Registrarse</title>
+    <!-- Estilos de CSS -->
+</head>
+
+<body>
+    <!-- Incio de Encabezado -->
+    <header></header>
+    <!-- Fin de encabezado -->
+
+    <!-- Incio del contenido principal -->
+    <div class="content">
+        <form action="" method="post" class="register-form">
+            <h2>Ingrese sus datos</h2>
+            <input type="email" name="" id="" class="register-form__input" placeholder="" required>
+            <input type="password" name="" id="" class="register-form__input" placeholder="" required>
+            <label for="name">Nombre:</label>
+            <input type="text" name="" class="register-form__input" placeholder="" required>
+            <label for="First-surname">Apellido paterno:</label>
+            <input type="text" name="" class="register-form__input" placeholder="" required>
+            <label for="Second-surname">Apellido materno:</label>
+            <input type="text" name="" class="register-form__input" placeholder="" required>
+            <label for="fecha-nacimiento">Fecha de nacimiento:</label>
+            <input type="date" name="" id="" class="register-form__input" placeholder="" required>
+            <div class="nested-content">
+                <label for="radio">Sexo:</label>
+                <label for="generoF">F</label>
+                <input type="radio" name="genero" id="" class="register-form__input" value="femenino">
+                <label for="generoM">M</label>
+                <input type="radio" name="genero" id="" class="register-form__input" value="masculino">
+            </div>
+            <label for="telefono">Numero de telefono:</label>
+            <input type="tel" name="" id="" class="register-form__input" placeholder="" required>
+            <label for="cm">Estatura:</label>
+            <input type="text" name="" id="" class="register-form__input"  placeholder="" required>
+            <label for="gr">Peso:</label>
+            <input type="tex" name="" id="" class="register-form__input" placeholder="" required>
+            <button type="submit" name="" class="register-btn">Registrarse</button>
+        </form>
+    </div>
+    <!-- Fin del contenido principal -->
+    
+    <!-- Inicio del pie de pagina -->
+    <footer></footer>
+    <!-- Fin del pie de pagina -->
+</body>
+
+</html>
