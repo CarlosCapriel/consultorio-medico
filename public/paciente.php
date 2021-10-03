da70928a5aeca7dec7620b406f5ee2227c884c84
Reyna <al060872@uacam.mx>
2021-10-02 22:08:22 -0500

    se modifico los arcchivos de la carpeta public

--- public/paciente.php
+++ public/paciente.php
@@ -1,3 +1,7 @@
+<!-- Estilos de CSS -->
+<link rel="stylesheet" href="public/css/style_paciente.css">
+
+<!-- contenedor -->
 <div class="row">
 
     <div class="col s12 ">
@@ -67,33 +71,4 @@
 
     </div>
 </div>
-
-<script>
-    'use strict'
-    
-    window.addEventListener('load', function () {
-        var select = document.querySelector("#option");
-        var i = "";
-        var input = document.querySelector("#input");
-    
-        select.addEventListener('change', function (e) {
-            e.preventDefault();
-            input.innerHTML = '';
-            for (i = 0; i < select.value; i++) {
-                createInputs();
-            }
-        });
-    
-        function createInputs() {
-            var element = document.createElement('div');
-            element.innerHTML = `
-            <div class="row">
-                <p>Escriba aqui: </p>
-                <input type="text" class="form-control" />
-            </div>
-        `;
-            input.appendChild(element);
-            //${i + 1}
-        }
-    });
-</script>
+<script src="public/js/paciente_select.js"></script>
