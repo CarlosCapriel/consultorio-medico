fcc6eb9afa49e48fabdfcec83e976e9ad74bfeda
Reyna <al060872@uacam.mx>
2021-10-28 15:23:09 -0500

    Se reviso la gramatica del proyecto

--- public/home.php
+++ public/home.php
@@ -1,69 +1,68 @@
-  <!-- Estilos en CSS -->
-  <link rel="stylesheet" href="public/css/style_home.css">
+<!-- Estilos en CSS -->
+<link rel="stylesheet" href="public/css/style_home.css">
 
 <!-- Contenido de la pagina principal -->
 <div class="row | header">
-        <div class="section | col s12 m8">
-            <!-- Note that "m4 l3" was added -->
-            <!-- Grey navigation panel -->
-    
-            <h2 class="header__title">Bienvenido</h2>
-            <p class="header__content">Cuidandote desde casa, te ofrecemos un servicio de calidad 
-                ajustandose a una nueva normalidad ahora puedes agendar tus consultas medicas en 
-                linea, porque procuramos tu salud y queremos que estes bien registarte ahora con 
-                nosotros y cuidemonos todos. 
-            </p>
-        </div>
-    </div>
-    <div class="container">
-        <div class="row">
 
-            <div class="col s4">
-                <!-- Promo Content 1 goes here -->
-                <div class="card white darken-1 hoverable">
-                    <div class="card-content black-text">
-                        <span class="card-title">Mision</span>
-                        <p>Realizar un sitio web para ayudar a todas las personas que arriesgan su 
-                            salud, así mismo reduciendo completamente el tiempo de espera.</p>
-                    </div>
-                    <div class="card-action">
-                        <a href="#">Mas información</a>
-                        
-                    </div>
-                </div>
-            </div>
-            <div class="col s4">
-                <!-- Promo Content 2 goes here -->
-                <div class="card white darken-1 hoverable">
-                    <div class="card-content black-text">
-                        <span class="card-title">Atencion</span>
-                        <p>Te queremos brindar tdodos nuestro servicios con la mejor calidad, por eso
-                            nuestros horarios se ajustan a tus necesidades, te ofrecemos horarios de consulta 
-                            flexibles
-                        </p>
-                    </div>
-                    <div class="card-action">
-                        <a href="#">Mas información</a>
-                        
-                    </div>
-                </div>
-            </div>
-            <div class="col s4">
-                <!-- Promo Content 3 goes here -->
-                <div class="card white darken-1 hoverable">
-                    <div class="card-content black-text">
-                        <span class="card-title">Vision</span>
-                        <p>Proporcionarte un servicio de calidad que se ajuste a las nuevas regulaciones de
-                            salud y que garantize tu seguridad incluso antes de salir de casa
-                        </p>
-                    </div>
-                    <div class="card-action">
-                        <a href="#">Mas información</a>
-                        
-                    </div>
-                </div>
-            </div>
-
-        </div>
+    <div class="section | col s12 m8">
+        <!-- Note that "m4 l3" was added -->
+        <!-- Grey navigation panel -->
+        <h2 class="header__title">Bienvenido</h2>
+        <p class="header__content">Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+            Consectetur unde facere voluptas delectus iure vitae provident, nobis est, nesciunt
+            cumque amet corrupti ad minima quis reiciendis, nostrum sit sequi dignissimos?
+        </p>
     </div>
+</div>
+<div class="container">
+    <div class="row">
+
+        <div class="col s4">
+            <!-- Promo Content 1 goes here -->
+            <div class="card white darken-1 hoverable">
+                <div class="card-content black-text">
+                    <span class="card-title">Mision</span>
+                    <p>Realizar un sitio web para ayudar a todas las personas que arriesgan su
+                        salud, así mismo reduciendo completamente el tiempo de espera.</p>
+                </div>
+                <div class="card-action">
+                    <a href="#">Mas información</a>
+
+                </div>
+            </div>
+        </div>
+        <div class="col s4">
+            <!-- Promo Content 2 goes here -->
+            <div class="card white darken-1 hoverable">
+                <div class="card-content black-text">
+                    <span class="card-title">Atencion</span>
+                    <p>Te queremos brindar tdodos nuestro servicios con la mejor calidad, por eso
+                        nuestros horarios se ajustan a tus necesidades, te ofrecemos horarios de consulta
+                        flexibles
+                    </p>
+                </div>
+                <div class="card-action">
+                    <a href="#">Mas información</a>
+
+                </div>
+            </div>
+        </div>
+        <div class="col s4">
+            <!-- Promo Content 3 goes here -->
+            <div class="card white darken-1 hoverable">
+                <div class="card-content black-text">
+                    <span class="card-title">Vision</span>
+                    <p>Proporcionarte un servicio de calidad que se ajuste a las nuevas regulaciones de
+                        salud y que garantize tu seguridad incluso antes de salir de casa
+                    </p>
+                </div>
+                <div class="card-action">
+                    <a href="#">Mas información</a>
+
+                </div>
+            </div>
+        </div>
+
+    </div>
+</div>
 
