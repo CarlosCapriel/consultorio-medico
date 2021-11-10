f64657d0c41d4dbd65fea58d05c62951dc1292c5
Carlos-Capriel <ccapriel@auronix.com>
2021-11-10 02:30:33 -0600

    rediseÃnio tabala historial de citas,centrado,color

--- public/historialcitas.php
+++ public/historialcitas.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 require_once "./db/conexion/Conexion.php";
 
     $cadena = $_SESSION['id_paciente'];
@@ -8,36 +8,34 @@ require_once "./db/conexion/Conexion.php";
     $consulta = "SELECT id_cita,fecha_cita,hora,estatus FROM citas where id_paciente = $cadena";
     $sql = $conexion->obtenerDatos($consulta);
 ?>
-<table width="700" cellpadding="2" cellspacing="0" bordercolor="#CCCCCC">
-<tr>
-    <td>Cita</td>    
-    <td>Fecha</td>
-    <td>Hora</td>
-    <td>Estatus</td>
-</tr>
-
-<?php 
-    for($i=0; $i < count($sql); $i++){
-      
-    
-?>
-    <tr>
-        <td><?php echo $i+1?></td>
-        <td><?php echo $sql[$i]['fecha_cita']; ?></td>
-        <td><?php echo $sql[$i]['hora']; ?></td>
-        <?php 
-            if($sql[$i]['estatus']==1){ ?>
-            <td>En espera</td>
-        <?php
-            }else{ ?>
-            <td>Finalizada</td>
-        <?php        
-            }
-        ?>
+<div class="container">
+<table width="700" cellpadding="2" cellspacing="0" bordercolor="#CCCCCC" class="striped">
+    <tr class="teal darken-4 white-text">
+        <td>Cita</td>    
+        <td>Fecha</td>
+        <td>Hora</td>
+        <td>Estatus</td>
     </tr>
+    <?php
+        for ($i=0; $i < count($sql); $i++) {
+            ?>
+        <tr>
+            <td><?php echo $i+1?></td>
+            <td><?php echo $sql[$i]['fecha_cita']; ?></td>
+            <td><?php echo $sql[$i]['hora']; ?></td>
+            <?php
+                if ($sql[$i]['estatus']==1) { ?>
+                <td>En espera</td>
+            <?php
+                } else { ?>
+                <td>Finalizada</td>
+            <?php
+                } ?>
+        </tr>
 
-<?php 
-}
-?>
+    <?php
+        }
+    ?>
 </table>
+</div>
 
