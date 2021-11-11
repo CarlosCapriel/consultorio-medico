f273207fc14c6cd003e536bc7687cf8acb76ab34
Reyna <al060872@uacam.mx>
2021-11-11 00:51:46 -0600

    Se añadio una ventana de inicio a medico

--- public/HomeMedico.php
+++ public/HomeMedico.php
@@ -1,4 +1,4 @@
-<div>
+<div class="container">
     <h2>Iniciamos un dia lleno de actitud.</h2>
     <p>Recordemos iniciar un dia lleno de actitud,
         el servicio es una de los mas grandes privilegios.
--- public/medico.php
+++ public/medico.php
@@ -1,6 +1,12 @@
 <!-- Estilos en CSS -->
 <link rel="stylesheet" href="public/css/style_medico.css">
 
-<div class="row">
-    <h1>Doctor bienvenido a su panel de control</h1>
+<!-- Bienvenida al medico -->
+<div class="row" >
+    
+    <h1 style="padding-left: 2rem">Doctor bienvenido a su panel de control</h1>
+    <hr>
 </div>
+<!-- Crear Receta Medica 
+//Es la interfaz que estara por defecto al inicio de sesion del medico -->
+ <?php include 'public/FormRecetas.php' ?>
