bc3f9adaad279d73cc3b8df37f9d0d1823b6a0a7
Reyna <al060872@uacam.mx>
2021-11-12 22:27:35 -0600

    Se reagrego las modificaciones en la clase medico

--- public/medico.php
+++ public/medico.php
@@ -1,6 +1,8 @@
 <!-- Estilos en CSS -->
-<link rel="stylesheet" href="public/css/style_medico.css">
+<link rel="stylesheet" href="public/css/style_mdc.css">
 
-<div class="row">
+<div class="row welcome">
     <h1>Doctor bienvenido a su panel de control</h1>
 </div>
+<!-- Vista inicial al logearse como medico - Rutea a Crear la recetas Medica -->
+<?php include 'public/FormRecetas.php' ?>
