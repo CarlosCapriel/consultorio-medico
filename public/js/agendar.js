82743e33e5fba8d1a93f2ab50e1fe04a591186ba
Sarahi <al060884@uacam.mx>
2021-11-12 22:27:56 -0600

    Valide agendar

---
+++ public/js/agendar.js
@@ -0,0 +1,22 @@
+    var d = new Date();
+    
+    document.addEventListener('DOMContentLoaded', function() {
+        var elems = document.querySelectorAll('.datepicker');
+        var instances = M.Datepicker.init(elems, {
+            format : 'yyyy-mm-dd',
+            yearRange: 1,
+            firstDay:1,
+            disableWeekends: true,
+            minDate: new Date(d.setDate(d.getDate() +1)),
+            maxDate: new Date(d.setDate(d.getDate() + 90)),
+            i18n:{
+                months: ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre',
+            'Octubre', 'Noviembre', 'Diciembre'],
+            weekdaysAbbrev: ['D', 'L', 'M', 'M','J','V','S'],
+            
+            }
+
+
+        });
+    });
+
