6a6769500e5e33c45785a700cb4ae46a02fbdc2c
Reyna <al060872@uacam.mx>
2021-11-21 21:33:06 -0600

    Devolvi el sesion

--- db/validar.php
+++ db/validar.php
@@ -11,8 +11,8 @@ $contrasenia=$_POST['contrasenia'];
 
     $consulta="SELECT*FROM login where correo='$email' and contrasenia='$contrasenia'";
         if ($sql = $conexion->obtenerDatos($consulta)) {
-          /* session_start();
-            Si se descomenta esta linea abra un error en pantalla sobre inicio de sesión*/
+           session_start();
+           /* Si se descomenta esta linea abra un error en pantalla sobre inicio de sesión*/
             $_SESSION['loggedUserName'] = $sql[0]['correo'] ;
             $_SESSION['id_rol'] = $sql[0]['id_rol'];
             if ($sql[0]['id_rol']==1) {
